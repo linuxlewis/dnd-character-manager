d2f96f9be904708c2ee15ad4beb8b7f9cb07299d
Sam Bolgert <sbolgert@gmail.com>
2026-03-09 10:25:07 -0500

    fix: Add missing closing TooltipProvider tag in StatBlock component

--- src/domains/character/ui/StatBlock.tsx
+++ src/domains/character/ui/StatBlock.tsx
@@ -56,6 +56,7 @@ export function StatBlock({ abilityScores, className }: StatBlockProps) {
 						</TooltipContent>
 					</Tooltip>
 				))}
-		</fieldset>
+			</fieldset>
+		</TooltipProvider>
 	);
 }
