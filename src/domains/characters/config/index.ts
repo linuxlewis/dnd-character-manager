14fd00298b1d480cba7c994efd46b04da48c58b6
Sam Bolgert <sbolgert@gmail.com>
2026-09-04 00:55:15 -0500

    feat(characters): add attribute and roll domain foundation

--- src/domains/characters/config/index.ts
+++ src/domains/characters/config/index.ts
@@ -2,3 +2,15 @@ export {
 	DND_5E_EXPERIENCE_THRESHOLDS,
 	getCharacterExperienceProgress,
 } from "./character-experience.js";
+export type { CharacterAbilityConfig, CharacterSkillConfig } from "./character-attributes.js";
+export {
+	ABILITY_CONFIG,
+	CHARACTER_ABILITIES,
+	CHARACTER_SKILLS,
+	DND_5E_PROFICIENCY_BONUSES,
+	getProficiencyBonus,
+	getSkillAbility,
+	SKILL_CONFIG,
+	SKILL_TO_ABILITY,
+} from "./character-attributes.js";
+export { buildCharacterAttributes, buildRollReference } from "./character-rolls.js";
